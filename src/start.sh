1b408a30ad49f508e75e64f798ab1bb04ee86a81
陈先生 <56025192@qq.com>
2022-12-18 23:54:49 +0800

    更新 start.sh

--- src/start.sh
+++ src/start.sh
@@ -8,22 +8,6 @@ echo "Debug = false" >> /core/etc/conf.ini
 echo "[Database]" >> /core/etc/conf.ini
 echo "DBFile = /core/db/cloudreve.db" >> /core/etc/conf.ini
 fi
-pem="/core/ssl.pem"
-if [ ! -f "$pem" ];then
-touch $pem
-echo "hello liyang" > $pem
-echo "文件创建完成"
-else
-echo "文件已经存在"
-fi
-key="/core/ssl.key"
-if [ ! -f "$key" ];then
-touch $key
-echo "hello liyang" > $key
-echo "文件创建完成"
-else
-echo "文件已经存在"
-fi
 /bin/chmod +x /core/cloudreve
 # nohup /bin/aria2c --conf /core/aria2/conf/aria2.conf > /core/log/aria2.log 2>&1 &
 /core/cloudreve -c /core/etc/conf.ini > /core/log/cloudreve.log
